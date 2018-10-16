43f1c99a58f597fa4382fae2ee07c2baec6aadab
migallotti <michele.gallotti@objectway.it>
2018-10-16 16:40:05 +0200

    Table component added -first version

--- src/components/table-component/index.ts
+++ src/components/table-component/index.ts
@@ -1,13 +1,23 @@
 import { PolymerElement } from '@polymer/polymer/polymer-element';
+import "@polymer/polymer/lib/elements/dom-repeat";
 
 import {html} from "@polymer/polymer/polymer-element";
 import * as view from "./table.template.pug";
-
-
+import * as dataMock from "./mock-data/mockData.json";
+import * as configMock from "./mock-data/mockConfiguration.json";
 
 export class TableComponent extends PolymerElement {
+    private data;
+    private config;
+
     constructor() {
         super();
+        this.data = dataMock;
+        this.config = configMock;
+
+        console.log('LOGGGGGG', this.data);
+        console.log('LOGGGGGG', this.config);
+
     }
 
     static get template() {
